f31651cd45615f567cf4360bbb3f840dce61f9d4
TanishqKT <135196289+tanishqkt03@users.noreply.github.com>
2025-04-25 16:16:23 +0530

    bug fixes

--- run.bat
+++ run.bat
@@ -8,6 +8,6 @@ REM Install dependencies
 pip install -r requirements.txt
 
 REM Start Flask server
-start http://127.0.0.1:5000
+@REM start http://127.0.0.1:5000
 start web\basic.html
 python web\server.py
